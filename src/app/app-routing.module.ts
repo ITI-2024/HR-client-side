4764911fd9d2448cb3930c1d8cefb0b355e80447
Habiba Elhadary <habibaelhadary991@gmail.com>
2024-05-03 01:07:06 +0300

    create login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { PublicSettingComponent } from './components/public-setting/public-setti
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import { EmployessComponent } from './components/employess/employess.component';
 import { HolidysComponent } from './components/holidys/holidys.component';
+import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
   { path: '', component: EmployessComponent },
   { path: 'holidays', component: HolidysComponent },
   { path: 'public-setting', component: PublicSettingComponent },
   { path: '**', component: NotFoundComponent },
+  {path:'login', component: LoginComponent},
 ];
 
 @NgModule({
---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,86 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { LoginService } from 'src/app/services/login.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  user:any;
+  myerror:any;
+  email: string = ''; 
+  password: string = '';
+  
+  constructor(public userServies: LoginService,public router:Router){
+
+  }
+  ngOnInit(): void {
+      
+
+  }
+
+  emailFormatValidator(control: FormControl): { [key: string]: any } | null {
+    const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    if (control.value && !emailPattern.test(control.value)) {
+      return { 'invalidEmail': true };
+    }
+    return null;
+  }
+  // Custom validator function for password format 
+  passwordFormatValidator(control: FormControl): { [key: string]: any } | null {
+    const passwordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{5,}$/;
+    if (control.value && !passwordPattern.test(control.value)) {
+      return { 'invalidPassword': true };
+    }
+    return null;
+  }
+
+  loginForm = new FormGroup({
+
+    email: new FormControl(null,
+      [
+        Validators.required,
+        this.emailFormatValidator
+      ]
+    ),
+    password: new FormControl(null,
+      [
+        Validators.required,
+        this.passwordFormatValidator
+       
+      ]
+    )
+  });
+  get getEmail(){
+    return this.loginForm.controls['email'];
+  }
+  get getPassword(){
+    return this.loginForm.controls['password'];
+  }
+  loginHandler(e: any){
+    e.preventDefault();
+    this.userServies.getlogin({ 
+    email: this.loginForm.get('email')?.value,
+    password: this.loginForm.get('password')?.value}).subscribe({
+      next: data=>{
+      this.user = data;
+      //Static 
+      if(!this.user.token){
+        localStorage.setItem('username', this.user.username);
+        this.router.navigate([' ']);
+      }
+
+    },
+      error: data=>{
+        this.myerror=data.error;
+        console.log(this.myerror);
+
+      }
+
+    });
+  }
+
+}
