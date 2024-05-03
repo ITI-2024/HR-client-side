b652911b8bd29de3af7ee0f4cb4d7439b7f1f101
batoultorky <batoultorky2001@hotmail.com>
2024-05-03 15:57:29 +0300

    retrieve employee data and linking delete with sweetalert

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,7 @@ import { PublicSettingComponent } from './components/public-setting/public-setti
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import { EmployeesComponent } from './components/employees/employees.component';
 import { HolidysComponent } from './components/holidys/holidys.component';
-
 import { AddPermissionComponent } from './components/add-permission/add-permission.component';
-
 import { LoginComponent } from './components/login/login.component';
 import { AttendanceComponent } from './components/attendance/attendance.component';
 import { SalaryReportComponent } from './components/salary-report/salary-report.component';
@@ -21,7 +19,7 @@ const routes: Routes = [
   { path: 'holidays', component: HolidysComponent },
   { path: 'public-setting', component: PublicSettingComponent },
   { path: 'employees', component:  EmployeesComponent },
-  /*{ path: '**', component: NotFoundComponent },*/
+  { path: '**', component: NotFoundComponent },
   { path: 'login', component: LoginComponent },
   { path: 'permissions', component: AddPermissionComponent },
 ];
---
+++ src/app/services/employees.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmployeesService {
+
+  constructor(public http: HttpClient) { }
+  baseUrl = "https://localhost:7291/api/Employee";
+  getAllEmployees() {
+    return this.http.get(this.baseUrl);
+  }
+  getEmployee(id: any) {
+    return this.http.get(`${this.baseUrl}/${id}`);
+  }
+  deleteEmployee(id: any) {
+    return this.http.delete(`${this.baseUrl}/employees/${id}`);
+  }
+  AddEmployee(employee:any)
+  {
+    return this.http.post(this.baseUrl,employee);
+  }
+ 
+}
+
