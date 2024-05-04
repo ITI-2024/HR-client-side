07637a4b4c84541651fdc3f5ac553acf91098f8b
Omnialwy <omniaalwy.11@gmail.com>
2024-05-04 11:37:02 +0300

    add department services and handle setting pop up form errors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { PublicSettingService } from './services/public-setting.service';
+import { DepartmentService } from './services/department.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
   publicSetting: any;
-  constructor(public publicSettingsSerivices: PublicSettingService) { }
+  departments: any;
+  constructor(public publicSettingsSerivices: PublicSettingService, public deparmentsServices: DepartmentService) { }
+  ngOnInit(): void {
+    this.deparmentsServices.getDepartments().subscribe({
+      next: data => {
+        this.departments = data;
+        if (this.departments.length == 0) {
+          this.deparmentsServices.addDepartment({
+            "name": "Development"
+          }).subscribe({
+            next: data => console.log(data)
+
+          });
+          this.deparmentsServices.addDepartment({
+            "name": "Sales"
+          }).subscribe({
+            next: data => console.log(data)
+
+          });
+          this.deparmentsServices.addDepartment({
+            "name": "Marketting"
+          }).subscribe({
+            next: data => console.log(data)
+
+          });
+        }
+      }
+    })
+
+  }
+
   addpublicSetting(p: any) {
     this.publicSettingsSerivices.addPublicSetting(p).subscribe({
       next: data => console.log(data)
---
+++ src/app/services/department.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DepartmentService {
+  baseUrl: string = 'https://localhost:7291/api/Department';
+  constructor(public http: HttpClient) { }
+  getDepartments() {
+    return this.http.get(this.baseUrl);
+  }
+  addDepartment(department: any) {
+    return this.http.post(this.baseUrl, department);
+  }
+}
