065f88293003f06e86851548c61daf89e1bd6b6d
batoultorky <batoultorky2001@hotmail.com>
2024-05-04 23:26:27 +0300

    add employee data through the form and update design

--- src/app/components/add-employee/add-employee.component.ts
+++ src/app/components/add-employee/add-employee.component.ts
@@ -13,36 +13,40 @@ export class AddEmployeeComponent implements OnInit {
 
   addempform: FormGroup;
   departments: any;
-  /* to know if it is add or edit */
   employeeId: any;
 
   constructor(
-    private formBuilder: FormBuilder , public x:DepartmentService , 
+    private formBuilder: FormBuilder,
+    public x: DepartmentService,
     public activatedRoute: ActivatedRoute,
     public employeesService: EmployeesService,
-    public router: Router) {
+    public router: Router
+  ) {
     this.addempform = this.formBuilder.group({
-      Name: [''],
-      Address: [''],
-      Phone: [''],
-      Gender: [''],
-      Nationality: [''],
-      Birthdate: [''],
-      Id: [''],
-      Contract: [''],
-      Salary: [''],
-      Arrive: [''],
-      Leave: ['']
+      name: [''],
+      address: [''],
+      phoneNumber: [''],
+      gender: [''],
+      nationality: [''],
+      birthDate: [''],
+      id: [''],
+      contractDate: [''],
+      salary: [''],
+      arrivingTime: [''],
+      leavingTime: [''],
+      idDept: ['']
     });
- 
   }
+
   ngOnInit(): void {
-    /*capture employee ID */
     this.employeeId = this.activatedRoute.snapshot.params['id'];
+    this.loadDepartments();
+  }
+
+   loadDepartments() {
     this.departments = this.x.getDepartments().subscribe({
       next: (data) => {
         this.departments = data;
-        console.log(this.departments);
       },
       error: (error) => {
         console.log(error);
@@ -50,7 +54,49 @@ export class AddEmployeeComponent implements OnInit {
     });
   }
 
+  employeeHandler(e: any) {
+    e.preventDefault();
+    if (this.employeeId == 0) {
+      const formData = this.addempform.value;
+      formData.arrivingTime = this.convertToTimeSpan(formData.arrivingTime);
+      formData.leavingTime =this.convertToTimeSpan(formData.leavingTime);
+      // Parse idDept to a number
+      formData.idDept = parseInt(formData.idDept, 10);
+      // Add new employee
+      console.log(formData)
+      this.employeesService.AddEmployee(formData).subscribe({
+        next: (data) => {
+          console.log(data)
+          this.router.navigate(['/employees']);
+        },
+        error: (error) => {
+          console.log(error);
+        }
+      });
+    } else {
+      console.log(this.addempform.value);
+      // Edit existing employee 
+      this.employeesService.getEmployee(this.employeeId, this.addempform.value).subscribe({
+        next: () => {
+          this.router.navigate(['/employees']);
+        },
+        error: (error) => {
+          console.error(error);
+        }
+      });
+    }
+  }
+
+  convertToTimeSpan(inputValue: string): string {
+    const parts = inputValue.split(':');
+    const hours = parseInt(parts[0], 10);
+    const minutes = parseInt(parts[1], 10);
+
+    const timeSpan = `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:00`;
+    return timeSpan;
+  }
   resetForm() {
     this.addempform.reset();
   }
 }
+
