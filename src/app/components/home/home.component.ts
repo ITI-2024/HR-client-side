eb9e67c86840c9c229cfa5dc68e7c783402c7a66
Omnialwy <omniaalwy.11@gmail.com>
2024-05-10 20:53:02 +0300

    add  download excel Sheet for Attendnence report

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PublicSettingService } from 'src/app/services/public-setting.service';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  /**
+   *
+   */
+  constructor(public settingServices: PublicSettingService) {
+  }
+  addPublicSetting(data: any) {
+    this.settingServices.addPublicSetting(data).subscribe({
+      next: data => console.log(data)
+    })
+  }
 
 }
