7005a7651170683786cd5dd8e6256193fa901a36
batoultorky <batoultorky2001@hotmail.com>
2024-05-14 02:53:34 +0300

    update sweet alert in permissions

--- src/app/components/roles/add-permission/add-permission.component.ts
+++ src/app/components/roles/add-permission/add-permission.component.ts
@@ -165,164 +165,180 @@ export class AddPermissionComponent implements OnInit {
 
   onSubmit(myForm: NgForm): void {
 
-    Swal.fire({
-      title: "Do you want to save the changes?",
-      showDenyButton: true,
-      showCancelButton: true,
-      confirmButtonText: "Save",
-      denyButtonText: `Don't save`
-    }).then((result) => {
-      /* Read more about isConfirmed, isDenied below */
-      if (result.isConfirmed) {
-        if (this.roleId == 0) {
-          this.permissions.forEach((prem: any, index: any) => {
-            let view = document.getElementById(`${prem.name}_view`) as HTMLInputElement;
-            let create = document.getElementById(`${prem.name}_create`) as HTMLInputElement;
-            let update = document.getElementById(`${prem.name}_update`) as HTMLInputElement;
-            let deelete = document.getElementById(`${prem.name}_delete`) as HTMLInputElement;
-            this.permissions[index].create = create.checked;
-            this.permissions[index].update = update.checked;
-            this.permissions[index].delete = deelete.checked;
-            this.permissions[index].view = view.checked;
+    if (this.roleId == 0) {
+      this.permissions.forEach((prem: any, index: any) => {
+        let view = document.getElementById(`${prem.name}_view`) as HTMLInputElement;
+        let create = document.getElementById(`${prem.name}_create`) as HTMLInputElement;
+        let update = document.getElementById(`${prem.name}_update`) as HTMLInputElement;
+        let deelete = document.getElementById(`${prem.name}_delete`) as HTMLInputElement;
+        this.permissions[index].create = create.checked;
+        this.permissions[index].update = update.checked;
+        this.permissions[index].delete = deelete.checked;
+        this.permissions[index].view = view.checked;
 
-            if (create.checked || update.checked || deelete.checked) {
-              this.permissions[index].view = true;
-            }
-          })
-          let getName = document.getElementById("roleName") as HTMLInputElement;
-          this.uniqueError = '';
-          this.validPemission = '';
-          const hasPermission = this.permissions.some((prem: any) => prem.view || prem.create || prem.update || prem.delete);
-          if (getName.value == null || getName.value == '') {
-            if (!hasPermission) {
-              Swal.fire({
-                icon: "error",
-                title: "Oops...",
-                text: "You must add group name and determine at least one permission",
-              });
-              return;
-            } else {
-              Swal.fire({
-                icon: "error",
-                title: "Oops...",
-                text: "You must add group first",
-              });
-              return;
-            }
-          } else if (!hasPermission) {
-            Swal.fire({
-              icon: "error",
-              title: "Oops...",
-              text: "You must determine at least one permission",
-            });
-            return;
-          }
-
-          let newrole = {
-            name: getName.value,
-            permissions: this.permissions
-          }
-          this.roleservice.getByName(getName.value).subscribe({
-            next: (data: any) => {
-              this.roleservice.addRole(newrole).subscribe({
-                next: (data: any) => {
-                  console.log(data);
-
-                  Swal.fire({
-                    title: 'Success!',
-                    text: 'Group Added Successfully',
-                    icon: 'success', // Custom icon HTML
-                    showCancelButton: false,
-                    confirmButtonColor: 'purple',
-                    confirmButtonText: 'Ok',
-
-                  }).then((result) => {
-                    result.isConfirmed ? this.router.navigate(['/roles']) : null
-                  })
-                },
-                error: (err: any) => {
-                  console.log(err)
-
-                }
-              });
-
-            },
-            error: (err: any) => {
-              console.log(err)
-              this.uniqueError = err.error;
-              if (err.error == null)
-                this.uniqueError = "Group Name Can't contain special character"
-            }
-          })
-          //end getbyname
-
-
+        if (create.checked || update.checked || deelete.checked) {
+          this.permissions[index].view = true;
         }
-        //end add function
-        //else update function
-        else {
-          console.log('add function')
-
-          this.permissions.forEach((prem: any, index: any) => {
-            let view = document.getElementById(`${prem.name}_view`) as HTMLInputElement;
-            let create = document.getElementById(`${prem.name}_create`) as HTMLInputElement;
-            let update = document.getElementById(`${prem.name}_update`) as HTMLInputElement;
-            let deelete = document.getElementById(`${prem.name}_delete`) as HTMLInputElement;
-            this.permissions[index].create = create.checked;
-            this.permissions[index].update = update.checked;
-            this.permissions[index].delete = deelete.checked;
-            this.permissions[index].view = view.checked;
-            // Check 'view' permission if any other permission is checked
-            if (create.checked || update.checked || deelete.checked) {
-              this.permissions[index].view = true;
+      })
+      let getName = document.getElementById("roleName") as HTMLInputElement;
+      this.uniqueError = '';
+      this.validPemission = '';
+      const hasPermission = this.permissions.some((prem: any) => prem.view || prem.create || prem.update || prem.delete);
+      if (getName.value == null || getName.value == '') {
+        if (!hasPermission) {
+          Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "You must add group name and determine at least one permission",
+          });
+          return;
+        } else {
+          Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "You must add group first",
+          });
+          return;
+        }
+      } else if (!hasPermission) {
+        Swal.fire({
+          icon: "error",
+          title: "Oops...",
+          text: "You must determine at least one permission",
+        });
+        return;
+      }else{
+        Swal.fire({
+          title: "Do you want to save the changes?",
+          showDenyButton: true,
+          showCancelButton: true,
+          confirmButtonText: "Save",
+          denyButtonText: `Don't save`
+        }).then((result) => {
+          /* Read more about isConfirmed, isDenied below */
+          if (result.isConfirmed) {
+            let newrole = {
+              name: getName.value,
+              permissions: this.permissions
             }
-          })
-
-          this.uniqueError = '';
-          this.validPemission = '';
-          const hasPermission = this.permissions.some((prem: any) => prem.view || prem.create || prem.update || prem.delete);
-          if (!hasPermission) {
-            this.validPemission = 'You must determine at least one permission';
-            return;
+            this.roleservice.getByName(getName.value).subscribe({
+              next: (data: any) => {
+                this.roleservice.addRole(newrole).subscribe({
+                  next: (data: any) => {
+                    console.log(data);
+      
+                    Swal.fire({
+                      title: 'Success!',
+                      text: 'Group Added Successfully',
+                      icon: 'success', // Custom icon HTML
+                      showCancelButton: false,
+                      confirmButtonColor: 'purple',
+                      confirmButtonText: 'Ok',
+      
+                    }).then((result) => {
+                      result.isConfirmed ? this.router.navigate(['/roles']) : null
+                    })
+                  },
+                  error: (err: any) => {
+                    console.log(err)
+      
+                  }
+                });
+      
+              },
+              error: (err: any) => {
+                console.log(err)
+                this.uniqueError = err.error;
+                if (err.error == null)
+                  this.uniqueError = "Group Name Can't contain special character"
+              }
+            })
+            Swal.fire("Saved!", "", "success");
+          } else if (result.isDenied) {
+            Swal.fire("Changes are not saved", "", "info");
           }
-          let getName = document.getElementById("roleName") as HTMLInputElement;
-          getName.value = this.roleName
-          console.log(this.roleName);
-          console.log(getName.value);
-          let newrole = {
-            name: getName.value,
-            permissions: this.permissions
-          }
-          this.roleservice.updateRole(newrole, this.decryptedId).subscribe({
-            next: (data: any) => {
-              console.log(data);
-
-              Swal.fire({
-                title: 'Success!',
-                text: 'Group updated Successfully',
-                icon: 'success', // Custom icon HTML
-                showCancelButton: false,
-                confirmButtonColor: 'purple',
-                confirmButtonText: 'Ok',
-
-              }).then((result) => {
-                result.isConfirmed ? this.router.navigate(['/roles']) : null
-              })
-            },
-            error: (err: any) => {
-              console.log(err)
-              console.log(getName.value);
-              console.log(newrole);
-            }
-          })
-
-
-        }//end else
-        Swal.fire("Saved!", "", "success");
-      } else if (result.isDenied) {
-        Swal.fire("Changes are not saved", "", "info");
+        });
       }
-    });
+      //end getbyname
+
+    }
+    //end add function
+    //else update function
+    else {
+      console.log('add function')
+
+      this.permissions.forEach((prem: any, index: any) => {
+        let view = document.getElementById(`${prem.name}_view`) as HTMLInputElement;
+        let create = document.getElementById(`${prem.name}_create`) as HTMLInputElement;
+        let update = document.getElementById(`${prem.name}_update`) as HTMLInputElement;
+        let deelete = document.getElementById(`${prem.name}_delete`) as HTMLInputElement;
+        this.permissions[index].create = create.checked;
+        this.permissions[index].update = update.checked;
+        this.permissions[index].delete = deelete.checked;
+        this.permissions[index].view = view.checked;
+        // Check 'view' permission if any other permission is checked
+        if (create.checked || update.checked || deelete.checked) {
+          this.permissions[index].view = true;
+        }
+      })
+
+      this.uniqueError = '';
+      this.validPemission = '';
+      const hasPermission = this.permissions.some((prem: any) => prem.view || prem.create || prem.update || prem.delete);
+      if (!hasPermission) {
+        Swal.fire({
+          icon: "error",
+          title: "Oops...",
+          text: "You must determine at least one permission",
+        });
+        return;
+      }else{
+        Swal.fire({
+          title: "Do you want to save the changes?",
+          showDenyButton: true,
+          showCancelButton: true,
+          confirmButtonText: "Save",
+          denyButtonText: `Don't save`
+        }).then((result) => {
+          /* Read more about isConfirmed, isDenied below */
+          if (result.isConfirmed) {
+            let getName = document.getElementById("roleName") as HTMLInputElement;
+            getName.value = this.roleName
+            console.log(this.roleName);
+            console.log(getName.value);
+            let newrole = {
+              name: getName.value,
+              permissions: this.permissions
+            }
+            this.roleservice.updateRole(newrole, this.decryptedId).subscribe({
+              next: (data: any) => {
+                console.log(data);
+      
+                Swal.fire({
+                  title: 'Success!',
+                  text: 'Group updated Successfully',
+                  icon: 'success', // Custom icon HTML
+                  showCancelButton: false,
+                  confirmButtonColor: 'purple',
+                  confirmButtonText: 'Ok',
+      
+                }).then((result) => {
+                  result.isConfirmed ? this.router.navigate(['/roles']) : null
+                })
+              },
+              error: (err: any) => {
+                console.log(err)
+                console.log(getName.value);
+                console.log(newrole);
+              }
+            })
+            Swal.fire("Saved!", "", "success");
+          } else if (result.isDenied) {
+            Swal.fire("Changes are not saved", "", "info");
+          }
+        });
+      }
+    }//end else
   }
 
 
